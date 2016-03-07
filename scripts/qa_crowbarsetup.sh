cf3d442b27a215a4dbdddfb104f5c3909beb3add
Bernhard M. Wiedemann <bwiedemann@suse.de>
2016-03-07 10:13:29 +0100

    qa_crowbarsetup: fix indent

--- scripts/qa_crowbarsetup.sh
+++ scripts/qa_crowbarsetup.sh
@@ -1117,9 +1117,9 @@ function onadmin_add_cloud_repo()
     safely zypper ar -f ${targetdir} Cloud
 
     if [ -n "${localreposdir_target}" ]; then
-      echo $CLOUDLOCALREPOS > /etc/cloudversion
+        echo $CLOUDLOCALREPOS > /etc/cloudversion
     else
-      cat "$targetdir/isoversion" > /etc/cloudversion
+        cat "$targetdir/isoversion" > /etc/cloudversion
     fi
 
     echo -n "This cloud was installed on `cat ~/cloud` from: " | \
